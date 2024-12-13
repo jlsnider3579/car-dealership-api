37f9a4b95787ac7848a03f84f3225d70308a9ac8
Jontez Snider <jlsnider3579@gmail.com>
2024-12-12 20:35:22 -0500

    Created sales contract interface class that will have another class implement its methods

--- dealership/src/main/java/com/pluralsight/dealership/LeaseContract.java
+++ dealership/src/main/java/com/pluralsight/dealership/LeaseContract.java
@@ -1,8 +1,8 @@
 package com.pluralsight.dealership;
 
 public class LeaseContract extends Contract {
-    private final double expectedEndingValuePercent = 0.05;
-    private final double leaseFeePercent = 0.07;
+    private double expectedEndingValuePercent = 0.05;
+    private double leaseFeePercent = 0.07;
     private double monthlyPayment;
 
     public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
@@ -14,18 +14,28 @@ public class LeaseContract extends Contract {
     }
 
     public double getLeaseFeePercent() {
+        leaseFeePercent = Math.round(getVehicleSold().getPrice() * 0.07);
         return leaseFeePercent;
     }
 
     @Override
     double getTotalPrice() {
         // TODO ADD LOGIC TO GET TOTAL PRICE FOR LEASE CONTRACT
-        return 0;
+        return  (getVehicleSold().getPrice() + leaseFeePercent) + (getVehicleSold().getPrice() * 0.04);
     }
 
     @Override
     double getMonthlyPayment() {
         // TODO ADD LOGIC TO GET MONTHLY PAYMENT FOR LEASE Contract
-        return 0;
+        return  Math.round((getTotalPrice() / 36) * 100.0) / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        return "LeaseContract{" +
+                "expectedEndingValuePercent=" + expectedEndingValuePercent +
+                ", leaseFeePercent=" + leaseFeePercent +
+                ", monthlyPayment=" + monthlyPayment +
+                '}';
     }
 }
---
+++ dealership/src/main/java/com/pluralsight/dealership/SalesContractDao.java
@@ -0,0 +1,11 @@
+package com.pluralsight.dealership;
+
+import java.util.List;
+
+public interface SalesContractDao {
+    List<SalesContract> findAllSalesContracts();
+    SalesContract findSalesContractById(int id);
+    void addSalesContract(SalesContract salesContract);
+    void updateSalesContract(SalesContract salesContract);
+    void deleteSalesContract(int id);
+}
